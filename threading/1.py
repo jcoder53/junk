ae764320eed0df50245765c6e8f2deaa5f6b186b
jcoder53 <jithins@iiitd.ac.in>
2020-04-27 19:27:06 +0530

    First file modified

--- threading/1.py
+++ threading/1.py
@@ -5,27 +5,27 @@ def greet():
     start = time.time()
     name = input("Enter your name: ")
     print(f"Hello {name}")
-    print(f"Time taken for greet: {time.time()-start}")
+    print(f"greet ran in {time.time()-start} seconds")
 
 def calculate():
     start = time.time()
-    print("Start calculating...")
     [x**2 for x in range(20000000)]
-    print(f"Time taken for calculate: {time.time()-start}")
+    print(f"calculate ran in {time.time()-start} seconds")
 
 start = time.time()
-#greet()
+greet()
 calculate()
-calculate()
-print(f"Total time taken is {time.time()-start}")
+print(f"The single thread execution took {time.time()-start} seconds")
 
-thread1 = Thread(target=calculate)
+thread1 = Thread(target=greet)
 thread2 = Thread(target=calculate)
 
 start = time.time()
-thread1.start()
+
 thread2.start()
+thread1.start()
 
 thread1.join()
 thread2.join()
-print(f"Time taken for two threaded: {time.time()-start}")
+
+print(f"The two threded execution took {time.time()-start} seconds")
